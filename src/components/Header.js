971c58b94742df5a761535a9a1ec19b407df9cda
Jonas Senft-Raiss <jonas.senftraiss@gmail.com>
2022-06-04 14:45:42 +0200

    cleaned up project structure

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className='flex flex-col items-center p-5'>
+            <h1 className='text-3xl text-indigo-200'>CURB YOUR MEMORY</h1>
+            <div className='italic'>The Music-Record Memory Game</div>
+        </div>
+    );
+}
+
+export default Header;
